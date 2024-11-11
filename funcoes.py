0a674c707b28ac2aede5d1bf3bf6242ce8c26297
jvca123 <jvca03122005@gmail.com>
2024-11-11 12:10:47 -0300

    Rastro erro rect vertical

--- funcoes.py
+++ funcoes.py
@@ -103,7 +103,7 @@ def desenha_p1(window,assets, posicao_inicial_x_p1, posicao_inicial_y_p1, moto_a
     
     moto_P1= assets['Moto_P1']
     moto_P1 = pygame.transform.scale(moto_P1, (MOTO_WIDTH, MOTO_HEIGHT))
-    moto_P1_rect= moto_P1.get_rect(topleft=(posicao_inicial_x_p1, posicao_inicial_y_p1))
+    moto_P1_rect= moto_P1.get_rect(center=(posicao_inicial_x_p1, posicao_inicial_y_p1))
     # Desenha a imagem na tela
     window.blit(moto_atual_P1, moto_P1_rect)
 
@@ -111,50 +111,50 @@ def desenha_p2(window, assets, posicao_inicial_x_p2, posicao_inicial_y_p2, moto_
     
     moto_P2=  assets['Moto_P2']
     moto_P2 = pygame.transform.scale(moto_P2, (MOTO_WIDTH, MOTO_HEIGHT))
-    moto_P2_rect= moto_P2.get_rect(topleft=(posicao_inicial_x_p2, posicao_inicial_y_p2))
+    moto_P2_rect= moto_P2.get_rect(center=(posicao_inicial_x_p2, posicao_inicial_y_p2))
     # Desenha a imagem na tela
     window.blit(moto_atual_P2, moto_P2_rect)
 
 #função pra mover a moto do P1
-def move_moto_P1(posicao_atual_x_P1, posicao_atual_y_P1, direcao_p1, WIDTH, HEIGHT, window, state):
-    posicao_atual_x_P1 += direcao_p1[0]
-    posicao_atual_y_P1 += direcao_p1[1]
+def move_moto_P1(posicao_atual_x_P1, posicao_atual_y_P1, direcao_P1, WIDTH, HEIGHT, window, state):
+    posicao_atual_x_P1 += direcao_P1[0]
+    posicao_atual_y_P1 += direcao_P1[1]
 
     # Verifique se a sprite bateu na borda da tela
     if posicao_atual_x_P1 < 0 or posicao_atual_x_P1 > WIDTH or posicao_atual_y_P1 < 0 or posicao_atual_y_P1 > HEIGHT:
         state["estado"]= False  # Retorna False se bater na borda e acaba o jogo
         return
     if posicao_atual_x_P1 < WIDTH / 2 - assets['tabuleiro1'].get_width() // 2 or posicao_atual_x_P1 > WIDTH / 2 + assets['tabuleiro1'].get_width() // 2 - 65:
-        posicao_atual_x_P1 -= direcao_p1[0]
+        posicao_atual_x_P1 -= direcao_P1[0]
 
     if posicao_atual_y_P1 < 100 or posicao_atual_y_P1 > assets['tabuleiro1'].get_height() + 31:
-        posicao_atual_y_P1 -= direcao_p1[1]
+        posicao_atual_y_P1 -= direcao_P1[1]
 
     posicao_atual_P1= [posicao_atual_x_P1, posicao_atual_y_P1]
     return posicao_atual_P1
     
 
 #função pra mover a moto do P2
-def move_moto_P2(posicao_atual_x_P2, posicao_atual_y_P2, direcao_p2, WIDTH, HEIGHT, window, state):
+def move_moto_P2(posicao_atual_x_P2, posicao_atual_y_P2, direcao_P2, WIDTH, HEIGHT, window, state):
     
-    posicao_atual_x_P2 += direcao_p2[0]
-    posicao_atual_y_P2 += direcao_p2[1]
+    posicao_atual_x_P2 += direcao_P2[0]
+    posicao_atual_y_P2 += direcao_P2[1]
 
     # Verifique se a sprite bateu na borda da tela
     if posicao_atual_x_P2 < 0 or posicao_atual_x_P2 > WIDTH or posicao_atual_y_P2 < 0 or posicao_atual_y_P2 > HEIGHT:
         state["estado"]= False  # Retorna False se bater na borda e acaba o jogo
         return
     if posicao_atual_x_P2 < WIDTH / 2 - assets['tabuleiro1'].get_width() // 2 or posicao_atual_x_P2 > WIDTH / 2 + assets['tabuleiro1'].get_width() // 2 - 65:
-        posicao_atual_x_P2 -= direcao_p2[0]
+        posicao_atual_x_P2 -= direcao_P2[0]
 
     if posicao_atual_y_P2 < 100 or posicao_atual_y_P2 > assets['tabuleiro1'].get_height() + 31:
-        posicao_atual_y_P2 -= direcao_p2[1]
+        posicao_atual_y_P2 -= direcao_P2[1]
 
     posicao_atual_P2= [posicao_atual_x_P2, posicao_atual_y_P2]
     return posicao_atual_P2
     
 #função para girar a moto do P1
-def gira_moto_P1(direcao_p1, moto_P1, posicao_atual_P1):
+def gira_moto_P1(direcao_P1, moto_P1, posicao_atual_P1):
     # Gira a imagem da moto e cria o rect correspondente
     moto_P1_baixo = pygame.transform.rotate(moto_P1, 90)
     moto_P1_cima = pygame.transform.rotate(moto_P1, -90)
@@ -162,13 +162,13 @@ def gira_moto_P1(direcao_p1, moto_P1, posicao_atual_P1):
     moto_P1_direita = pygame.transform.rotate(moto_P1, 180)
     
     # Seleciona a imagem e o rect com base na direção
-    if direcao_p1 == (0, velocidade_motos):
+    if direcao_P1 == (0, velocidade_motos):
         moto_atual_P1 = moto_P1_baixo
-    elif direcao_p1 == (0, -velocidade_motos):
+    elif direcao_P1 == (0, -velocidade_motos):
         moto_atual_P1 = moto_P1_cima
-    elif direcao_p1 == (velocidade_motos, 0):
+    elif direcao_P1 == (velocidade_motos, 0):
         moto_atual_P1 = moto_P1_direita
-    elif direcao_p1 == (-velocidade_motos, 0):
+    elif direcao_P1 == (-velocidade_motos, 0):
         moto_atual_P1 = moto_P1_esquerda
 
     # Cria o rect e centraliza-o na posição atual da moto 1
@@ -178,7 +178,7 @@ def gira_moto_P1(direcao_p1, moto_P1, posicao_atual_P1):
 
 
 #função para girar a moto do P2
-def gira_moto_P2(direcao_p2, moto_P2, posicao_atual_P2):
+def gira_moto_P2(direcao_P2, moto_P2, posicao_atual_P2):
     # Gira a imagem da moto e cria o rect correspondente
     moto_P2_baixo = pygame.transform.rotate(moto_P2, 90)
     moto_P2_cima = pygame.transform.rotate(moto_P2, -90)
@@ -186,13 +186,13 @@ def gira_moto_P2(direcao_p2, moto_P2, posicao_atual_P2):
     moto_P2_direita = pygame.transform.rotate(moto_P2, 180)
     
     # Seleciona a imagem e o rect com base na direção
-    if direcao_p2 == (0, velocidade_motos):
+    if direcao_P2 == (0, velocidade_motos):
         moto_atual_P2 = moto_P2_baixo
-    elif direcao_p2 == (0, -velocidade_motos):
+    elif direcao_P2 == (0, -velocidade_motos):
         moto_atual_P2 = moto_P2_cima
-    elif direcao_p2 == (velocidade_motos, 0):
+    elif direcao_P2 == (velocidade_motos, 0):
         moto_atual_P2 = moto_P2_direita
-    elif direcao_p2 == (-velocidade_motos, 0):
+    elif direcao_P2 == (-velocidade_motos, 0):
         moto_atual_P2 = moto_P2_esquerda
 
     # Cria o rect e centraliza-o na posição atual da moto 2
@@ -200,18 +200,30 @@ def gira_moto_P2(direcao_p2, moto_P2, posicao_atual_P2):
 
     return moto_atual_P2, moto_atual_P2_rect
 
-def update_rastro_P1(posicao_atual_x_P1, posicao_atual_y_P1, direcao_p1, rastro_list_P1):
+def update_rastro_P1(posicao_atual_P1, direcao_P1, rastro_list_P1):
+    '''if direcao_P1 == (0, velocidade_motos): #Direção para baixo
+        pos_pixel_rastro1 = (posicao_atual_P1[0], posicao_atual_P1[1])
+    elif direcao_P1 == (0, -velocidade_motos): #Direção para cima
+        pos_pixel_rastro1 = (posicao_atual_P1[0], posicao_atual_P1[1])
+    elif direcao_P1 == (velocidade_motos, 0): #Direção para direita
+        pos_pixel_rastro1 = (posicao_atual_P1[0], posicao_atual_P1[1]+14)
+    elif direcao_P1 == (-velocidade_motos, 0): #Direção para esquerda
+        pos_pixel_rastro1 = (posicao_atual_P1[0] + MOTO_WIDTH, posicao_atual_P1[1]+20)'''
 
+    rastro_list_P1.append(posicao_atual_P1)
+    if len(rastro_list_P1) > TAMANHO_RASTRO:
+        rastro_list_P1.pop(0)
 
-
-    rastro_list_P1.append((posicao_atual_x_P1, posicao_atual_y_P1))
-    rastro_list_P1.pop()
     return rastro_list_P1
 
-def update_rastro_P2 (posicao_atual_x_P2, posicao_atual_y_P2, direcao_p2, rastro_list_P2):
+def update_rastro_P2 (posicao_atual_P2, direcao_P2, rastro_list_P2):
 
-    rastro_list_P2.append()
-    rastro_list_P2.pop()
+    pos_pixel_rastro2 = posicao_atual_P2
+
+    rastro_list_P2.append(pos_pixel_rastro2)
+    if len(rastro_list_P2) > TAMANHO_RASTRO:
+        rastro_list_P2.pop(0)
+
     return rastro_list_P2
 
 pygame.quit()
--- jogo.py
+++ jogo.py
@@ -4,8 +4,8 @@ from funcoes import *
 
 
 window, state, assets = inicializa()
-direcao_p1= (0, velocidade_motos)
-direcao_p2= (0, -velocidade_motos)
+direcao_P1= (0, velocidade_motos)
+direcao_P2= (0, -velocidade_motos)
 
 posicao_atual_P1 = [posicao_inicial_x_P1, posicao_inicial_y_P1]
 posicao_atual_P2 = [posicao_inicial_x_P2, posicao_inicial_y_P2]
@@ -15,11 +15,11 @@ moto_P2_cima = pygame.transform.rotate(moto_P2, -90)
 
 rastro_skin_P1 = pygame.Surface((RASTRO_WIDTH, RASTRO_HEIGHT))
 rastro_skin_P1.fill(BLUE)
-rastro_P1_list = []
+rastro_list_P1 = []
 
 rastro_skin_P2 = pygame.Surface((RASTRO_WIDTH, RASTRO_HEIGHT))
 rastro_skin_P2.fill(ORANGE)
-rastro_P2_list = []
+rastro_list_P2 = []
 
 while state["estado"]:
     desenha(window, state, assets)
@@ -27,8 +27,8 @@ while state["estado"]:
 
     if state['tela_atual'] == TELA_DE_PLAY:
 
-        moto_atual_P1, moto_atual_P1_rect = gira_moto_P1(direcao_p1, moto_P1, posicao_atual_P1)
-        moto_atual_P2, moto_atual_P2_rect = gira_moto_P2(direcao_p2, moto_P2, posicao_atual_P2)
+        moto_atual_P1, moto_atual_P1_rect = gira_moto_P1(direcao_P1, moto_P1, posicao_atual_P1)
+        moto_atual_P2, moto_atual_P2_rect = gira_moto_P2(direcao_P2, moto_P2, posicao_atual_P2)
 
         desenha_p1 (window, assets, posicao_atual_P1[0], posicao_atual_P1[1], moto_atual_P1)
         desenha_p2 (window, assets, posicao_atual_P2[0], posicao_atual_P2[1], moto_atual_P2)
@@ -36,29 +36,29 @@ while state["estado"]:
         # Verifique quais teclas estão pressionadas para alterar a direção do P1
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:   # Esquerda
-            direcao_p1 = (-velocidade_motos, 0)
+            direcao_P1 = (-velocidade_motos, 0)
         elif keys[pygame.K_d]:  # Direita
-            direcao_p1 = (velocidade_motos, 0)
+            direcao_P1 = (velocidade_motos, 0)
         elif keys[pygame.K_w]:    # Cima
-            direcao_p1 = (0, -velocidade_motos)
+            direcao_P1 = (0, -velocidade_motos)
         elif keys[pygame.K_s]:  # Baixo
-            direcao_p1 = (0, velocidade_motos)
+            direcao_P1 = (0, velocidade_motos)
         
         # Verifique quais teclas estão pressionadas para alterar a direção do P2
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:   # Esquerda
-            direcao_p2 = (-velocidade_motos, 0)
+            direcao_P2 = (-velocidade_motos, 0)
         elif keys[pygame.K_RIGHT]:  # Direita
-            direcao_p2 = (velocidade_motos, 0)
+            direcao_P2 = (velocidade_motos, 0)
         elif keys[pygame.K_UP]:    # Cima
-            direcao_p2 = (0, -velocidade_motos)
+            direcao_P2 = (0, -velocidade_motos)
         elif keys[pygame.K_DOWN]:  # Baixo
-            direcao_p2 = (0, velocidade_motos)
+            direcao_P2 = (0, velocidade_motos)
 
         # Chame a função para mover as duas motos e verificar se está dentro do tabuleiro
-        posicao_atual_P1_2= move_moto_P1 (posicao_atual_P1[0], posicao_atual_P1[1], direcao_p1, WIDTH, HEIGHT, window, state)
+        posicao_atual_P1_2= move_moto_P1 (posicao_atual_P1[0], posicao_atual_P1[1], direcao_P1, WIDTH, HEIGHT, window, state)
         posicao_atual_P1 = posicao_atual_P1_2
-        posicao_atual_P2_2= move_moto_P2 (posicao_atual_P2[0], posicao_atual_P2[1], direcao_p2, WIDTH, HEIGHT, window, state)
+        posicao_atual_P2_2= move_moto_P2 (posicao_atual_P2[0], posicao_atual_P2[1], direcao_P2, WIDTH, HEIGHT, window, state)
         posicao_atual_P2 = posicao_atual_P2_2
 
         # Verifique se as motos bateram uma na outra
@@ -72,18 +72,20 @@ while state["estado"]:
             moto_atual_P2_rect = moto_atual_P2.get_rect(center=posicao_atual_P2)
             moto_atual_P1_rect.center = posicao_inicial_P1
             moto_atual_P2_rect.center = posicao_inicial_P2
+            rastro_list_P1 = []
+            rastro_list_P2 = []
 
-
-            rastro_list_P1 = update_rastro_P1()
-            rastro_lista_P2 = update_rastro_P2()
-
-        for rastro_P1_pos in rastro_list_P1:
-            window.blit(rastro_skin_P1, rastro_P1_pos)
+        pygame.draw.rect(window, (255,255,255), moto_atual_P1_rect)
+        # centro_p1 = (posicao_atual_P1[0] + moto_atual_P1_rect.width/2, posicao_atual_P1[1] + moto_atual_P1_rect.height/2)
+        rastro_list_P1 = update_rastro_P1(posicao_atual_P1, direcao_P1, rastro_list_P1)
+        rastro_list_P2 = update_rastro_P1(posicao_atual_P2, direcao_P2, rastro_list_P2)
 
         for rastro_P1_pos in rastro_list_P1:
             window.blit(rastro_skin_P1, rastro_P1_pos)
         
+        for rastro_P2_pos in rastro_list_P2:
+            window.blit(rastro_skin_P2, rastro_P2_pos)
+        
 
     # Atualiza a tela
     pygame.display.update()
-    
