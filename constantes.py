f4c3ac5c67e01ca057fbb234cde4cd2ee40eb9be
mldc007 <marlizeldc@al.insper.edu.br>
2025-09-07 16:16:57 -0300

    limpeza

--- constantes.py
+++ constantes.py
@@ -6,7 +6,6 @@ pygame.mixer.init()
 # FPS
 FPS = 60
 
-
 # CORES
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -19,37 +18,34 @@ ORANGE = (255, 165, 0)
 #fonte
 #fonte = pygame.font.Font(None, 50)
 
-
-#TELAS
-WIDTH, HEIGHT = 1920,1080
+# TELAS
+WIDTH, HEIGHT = 1920, 1080
 WIDTH_TAB, HEIGHT_TAB = 872, 868
 TELA_INICIAL = 0
 TELA_DE_PLAY = 1
-TELA_VENCEDOR_P1= 2
-TELA_VENCEDOR_P2= 3
+TELA_VENCEDOR_P1 = 2
+TELA_VENCEDOR_P2 = 3
 
 # Motos
 MOTO_WIDTH = 90
 MOTO_HEIGHT = 45
 
-
-# Rasotros
+# Rastros
 RASTRO_WIDTH, RASTRO_HEIGHT = 10, 10
 TAMANHO_RASTRO = 120
 
-
-#JOGADORES
-posicao_inicial_x_P1=560
-posicao_inicial_y_P1=200
-posicao_inicial_x_P2=1330  
-posicao_inicial_y_P2=900 
+# JOGADORES
+posicao_inicial_x_P1 = 560
+posicao_inicial_y_P1 = 200
+posicao_inicial_x_P2 = 1330  
+posicao_inicial_y_P2 = 900 
 posicao_inicial_P1= [posicao_inicial_x_P1, posicao_inicial_y_P1]
 posicao_inicial_P2= [posicao_inicial_x_P2, posicao_inicial_y_P2]
 
 velocidade_motos= 5
 
-pontos_jogador_P1=0
-pontos_jogador_P2=0
+pontos_jogador_P1 = 0
+pontos_jogador_P2 = 0
 
 direcao_P1= (0, velocidade_motos)
 direcao_P2= (0, -velocidade_motos)
@@ -71,14 +67,14 @@ tempo_decorrido = 0
 scoreboard = []
 
 assets = {
-    "tela_de_play" : pygame.image.load("image/TELA_PLAY_TRON.png"),
-    "titulo" : 'TRON LEGACY' ,
+    "tela_de_play": pygame.image.load("image/TELA_PLAY_TRON.png"),
+    "titulo": "TRON LEGACY",
     "tabuleiro1": pygame.image.load("image/tabuleiro1.png"),
     "tabuleiro2": pygame.image.load("image/tabuleiro2.png"),
     "Moto_P1": pygame.image.load("image/moto-azul.png"),
     "Moto_P2": pygame.image.load("image/moto-laranja.png"),
-    "P1_vencedor" : pygame.image.load("image/blue_wins.webp"),
-    "P2_vencedor" : pygame.image.load("image/orange_wins.webp"),
-    "boom_sound" :pygame.mixer.Sound('sons/tron-light-cycle-chase-sound-fx-1982-6jrpzais_MOV5ehTB.mp3')
+    "P1_vencedor": pygame.image.load("image/blue_wins.webp"),
+    "P2_vencedor": pygame.image.load("image/orange_wins.webp"),
+    "boom_sound":pygame.mixer.Sound('sons/tron-light-cycle-chase-sound-fx-1982-6jrpzais_MOV5ehTB.mp3')
 } 
 
